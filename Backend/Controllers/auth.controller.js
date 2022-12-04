136be85bd6589babc1ec140fca115126cd22b565
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-04 12:25:37 +0530

    auth updated

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -8,29 +8,24 @@ export const cookieOptions = {
     httpOnly: true,
     //could be in a separate file in utils
 }
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @parameters name, email, password
+ * @returns User Object
+ ******************************************************/
+ export const signUp = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body
 
-/***********************************
- * @SignUp
- * @ROUTE  http://localhost:5000/api/auth/SIGNUP
- * @DESCRIPTION user SIGNUP Controller
- * @PARAMETER
- * @RETURNS USER OBJECT
- * 
- */
-
-
-export const signUp = asyncHandler (async (req, res)=>{
-    const {name, email, password} = req.body;
-
-    if(!name || !email || !password){
+    if (!name || !email || !password) {
         throw new CustomError('Please fill all fields', 400)
     }
-
-    // check if user exists
+    //check if user exists
     const existingUser = await User.findOne({email})
 
-    if(existingUser) {
-        throw err
+    if (existingUser) {
+        throw new CustomError('User already exists', 400)  
     }
 
     const user = await User.create({
@@ -38,13 +33,11 @@ export const signUp = asyncHandler (async (req, res)=>{
         email,
         password
     });
-
-    const token = User.getJwtToken();
+    const token = user.getJwtToken()
     console.log(user);
-    user.password = undefined;
+    user.password = undefined
 
-
-    res.cookie("token", token, cookieOptions);
+    res.cookie("token", token, cookieOptions)
 
     res.status(200).json({
         success: true,
@@ -52,21 +45,62 @@ export const signUp = asyncHandler (async (req, res)=>{
         user
     })
 
+})
+
+/******************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/login
+ * @description User signIn Controller for loging new user
+ * @parameters  email, password
+ * @returns User Object
+ ******************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    if ( !email || !password) {
+        throw new CustomError('Please fill all fields', 400)
+    }
+
+    const user = User.findOne({email}).select("+password")
+
+    if (!user) {
+        throw new CustomError('Invalid credentials', 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if (isPasswordMatched) {
+        const token = user.getJwtToken()
+        user.password = undefined;
+        res.cookie("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError('Invalid credentials - pass', 400)
 
 })
 
 
-/***********************************
- * @LOGIN
- * @ROUTE  http://localhost:5000/api/auth/login
- * @DESCRIPTION user signIn Controller
- * @PARAMETER
- * @RETURNS
- * 
- */
-
-
-export const login = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    
+/******************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User logout bby clearing user cookies
+ * @parameters  
+ * @returns success message
+ ******************************************************/
+export const logout = asyncHandler(async (_req, res) => {
+    // res.clearCookie()
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
 })
