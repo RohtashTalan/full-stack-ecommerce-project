fae775a99049793ababe2cb078c2f3e4fd184f83
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-04 13:54:41 +0530

    islogdIn controller

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import User from '../Model/user.model';
+import  JWT, { JsonWebTokenError }  from 'jsonwebtoken';
+import CustomError from '../utils/customError';
+import asyncHandler from '../services/asyncHandler';
+import config from '../config/index';
+
+
+
+
+
+export const isLoggdIn = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if(
+        req.cookies.token || req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+    ){
+        token = req.cookies.token || req.headers.authorization.split(' ')[1]
+    }
+
+    if(!token){
+        throw new CustomError("Not authorized to access this route", 401)
+    }
+
+    try {
+      const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET)
+      // _id, find user based on id, set this in req.user
+
+    req.user = await User.findById(decodedJwtPayload._id, "name email role");
+    next();
+
+    } catch (err) {
+        throw new CustomError("Not authorized to access this route", 401)
+    }
+})
