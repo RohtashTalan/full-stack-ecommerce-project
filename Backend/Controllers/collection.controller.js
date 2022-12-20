161c8f13908ae9ba40bcb95ea266682e4a0e4465
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-20 19:42:34 +0530

    updated collection

--- Backend/Controllers/collection.controller.js
+++ Backend/Controllers/collection.controller.js
@@ -1,6 +1,6 @@
-import Collection  from '../Model/collection.schema';
-import asyncHandler from '../services/asyncHandler';
-import CustomError from '../utils/customError';
+import Collection from "../Model/collection.schema";
+import asyncHandler from "../services/asyncHandler";
+import CustomError from "../utils/customError";
 
 /******************************************************
  * @Create_COLLECTION
@@ -11,21 +11,57 @@ import CustomError from '../utils/customError';
  ******************************************************/
 
 export const createCollection = asyncHandler(async (req, res) => {
-    // name from frontend
-    const {name} = req.body;
-    if(!name) {
-        throw new CustomError("Collection name is required", 400);
-    }
+  // name from frontend
+  const { name } = req.body;
+  if (!name) {
+    throw new CustomError("Collection name is required", 400);
+  }
 
-    // add this name to database
-    const collection = await Collection.create({name})
+  // add this name to database
+  const collection = await Collection.create({ name });
 
-    // send this response value to frontend
-    res.status(200).json({
-        success: true,
-        message: "Collection created with success",
-        collection
+  // send this response value to frontend
+  res.status(200).json({
+    success: true,
+    message: "Collection created with success",
+    collection,
+  });
+});
 
-    })
-})
 
+
+/******************************************************
+ * @Update_COLLECTION
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @parameters name, email, password
+ * @returns User Object
+ ******************************************************/
+
+export const updateCollection = asyncHandler(async (req, res) => {
+  // existing value to be updated
+  const { id: collectionId } = req.params;
+
+  // new value to get updated
+  const { name } = req.body;
+
+  if (!name) {
+    throw new CustomError("Collection name is required", 400);
+  }
+
+  const updatedCollection = await Collection.findByIdAndUpdate(
+    collectionId,
+    { name },
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedCollection) {
+    throw new CustomError("Collection not found", 400);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Collection Value updated successfully",
+    updatedCollection,
+  });
+});
