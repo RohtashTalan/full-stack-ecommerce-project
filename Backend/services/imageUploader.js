9ecf5bb5581661b0c3dbca212523255333275876
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-20 20:39:36 +0530

    aws file upload

---
+++ Backend/services/imageUploader.js
@@ -0,0 +1,21 @@
+ import s3 from '../config/s3.config'
+
+
+
+ export const s3FileUpload = async({bucketName, key, body, contentType}) =>{
+    return await s3.upload({
+        Bucket: bucketName,
+        key: key,
+        Body: body,
+        ContentType: contentType
+    }).promise()
+ }
+
+// delete file from aws bucket
+ export const deleteFile  =async ({bucketName, key}) => {
+    return await s3.deleteObject({
+        Bucket: bucketName,
+        key: key
+    }).promise()
+
+ }
