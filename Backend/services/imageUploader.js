413fbafc3fb270a5196e6b23f1636dc19972fc82
Rohtash Talan <rohtashtalancompany@gmail.com>
2022-12-23 18:11:52 +0530

    format of document

--- Backend/services/imageUploader.js
+++ Backend/services/imageUploader.js
@@ -1,23 +1,23 @@
- import s3 from '../config/s3.config'
-
+import s3 from "../config/s3.config";
 
 // upload the file to aws s3 bucket
- export const s3FileUpload = async({bucketName, key, body, contentType}) =>{
-    return await s3.upload({
-        Bucket: bucketName,
-        key: key,
-        Body: body,
-        ContentType: contentType
-    }).promise()
- }
-
- 
+export const s3FileUpload = async ({ bucketName, key, body, contentType }) => {
+  return await s3
+    .upload({
+      Bucket: bucketName,
+      key: key,
+      Body: body,
+      ContentType: contentType,
+    })
+    .promise();
+};
 
 // delete file from aws bucket
- export const deleteFile  =async({bucketName, key}) => {
-    return await s3.deleteObject({
-        Bucket: bucketName,
-        key: key
-    }).promise()
-
- }
+export const deleteFile = async ({ bucketName, key }) => {
+  return await s3
+    .deleteObject({
+      Bucket: bucketName,
+      key: key,
+    })
+    .promise();
+};
