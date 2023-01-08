1feedeb0956235674fd79a91015de87380a0ff6a
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-08 09:31:22 +0530

    coupon controllers

--- Backend/Controllers/coupons.controller.js
+++ Backend/Controllers/coupons.controller.js
@@ -1,3 +1,11 @@
+import Coupon from '../Model/coupon.schema.js';
+import asyncHandler from "../services/asyncHandler.js";
+import CustomError from "../utils/customError.js";
+import config from "../config/index.js";
+
+
+
+
 /**********************************************************
  * @CREATE_COUPON
  * @route https://localhost:5000/api/coupon
@@ -5,7 +13,19 @@
  * @description Only admin and Moderator can create the coupon
  * @returns Coupon Object with success message "Coupon Created SuccessFully"
  *********************************************************/
+export const createCoupon = asyncHandler( async(req, res, next) => {
+    const {code, discount} = req.body;
+    
+    if(!code || !discount){throw new CustomError("Both Field Required", 401)}
 
+    const newCoupon = await Coupon.create({code, discount});
+
+    res.status(200).json({
+        success: true,
+        newCoupon
+    })
+
+})
 
 /**********************************************************
  * @DEACTIVATE_COUPON
@@ -14,7 +34,19 @@
  * @description Only admin and Moderator can update the coupon
  * @returns Coupon Object with success message "Coupon Deactivated SuccessFully"
  *********************************************************/
+export const deactiveCoupon = asyncHandler( async(req, res, next) => {
+    const {couponId} = req.params;
+    
+    if(!couponId){throw new CustomError("Coupon code Required", 401)}
 
+    const deactive = await Coupon.findByIdAndUpdate({_id: couponId}, {active:false}, {new: true});
+
+    res.status(200).json({
+        success: true,
+        deactive
+    })
+
+})
 
 /**********************************************************
  * @DELETE_COUPON
@@ -24,6 +56,23 @@
  * @returns Success Message "Coupon Deleted SuccessFully"
  *********************************************************/
 
+export const deleteCoupon = asyncHandler( async(req, res, next) => {
+    const {couponId} = req.params;
+    
+    if(!couponId){throw new CustomError("Coupon code Required", 401)}
+
+    const deletedCoupon = await Coupon.findByIdAndDelete({_id: couponId});
+
+    if(!deletedCoupon){
+        throw new CustomError("NO Coupon was found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        deletedCoupon
+    })
+
+})
 
 
 /**********************************************************
@@ -33,3 +82,20 @@
  * @description Only admin and Moderator can get all the coupons
  * @returns allCoupons Object
  *********************************************************/
+
+export const getCoupons = asyncHandler( async(req, res, next) => {
+    
+
+    const coupons = await Coupon.find({});
+
+    
+    if(!coupons){
+        throw new CustomError("NO Coupon was found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        coupons
+    })
+
+})
