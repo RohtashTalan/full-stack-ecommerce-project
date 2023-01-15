905745b6ad5012066add985aa1f0542f43aaa059
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-15 09:56:09 +0530

    components file added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline text-red-600">
       <h1>Ecommerce App</h1>
     </div>
   );
