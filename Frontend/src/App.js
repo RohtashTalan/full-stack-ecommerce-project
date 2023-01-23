66f1bdc1ca8e9f0fde590f2a1c45d7ed5fd8411f
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-23 08:01:36 +0530

    header building started

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,16 @@
-
+import Header from "./Components/Header";
 
 function App() {
 
-  return (
-    <div className="text-3xl font-bold underline text-red-600">
-      <h1>Ecommerce App</h1>
-    </div>
+  return (<>
+
+  <Header />
+   <div className="text-3xl font-bold underline text-red-600">
+
+<h1>Ecommerce App</h1>
+</div>
+  </>
+   
   );
 }
 
--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logo from "../assests/img/logo.png";
 
 
 
@@ -7,10 +7,19 @@ const Header = () => {
 
 
     return(
-        <>
-        
-        
-        </>
+        <header className="bg-blue-700 h-20 text-white">
+            <div className="flex justify-center gap-20 items-center py-5">
+                <img className="h-12 w-40" src={logo} alt="Logo"/>
+                <div className="w-1/5"> <input type="text" /> <i class="fa-solid fa-magnifying-glass"></i></div>
+                <div className="flex justify-between items-center w-1/5">
+                    <button>Login</button>
+                    <div className="inline-flex">
+                    <i class="fa-solid fa-cart-shopping"></i> 
+                    <p>Cart</p>
+                        </div>
+                </div>
+            </div>
+        </header>
     )
 
 }
