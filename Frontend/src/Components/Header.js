a1e5b174ec000a20dfe22b2837cbd696432d703d
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-01-23 08:24:50 +0530

    header ready

--- Frontend/src/Components/Header.js
+++ Frontend/src/Components/Header.js
@@ -1,28 +1,28 @@
 import React from "react";
 import logo from "../assests/img/logo.png";
 
-
-
 const Header = () => {
-
-
-    return(
-        <header className="bg-blue-700 h-20 text-white">
-            <div className="flex justify-center gap-20 items-center py-5">
-                <img className="h-12 w-40" src={logo} alt="Logo"/>
-                <div className="w-1/5"> <input type="text" /> <i class="fa-solid fa-magnifying-glass"></i></div>
-                <div className="flex justify-between items-center w-1/5">
-                    <button>Login</button>
-                    <div className="inline-flex">
-                    <i class="fa-solid fa-cart-shopping"></i> 
-                    <p>Cart</p>
-                        </div>
-                </div>
-            </div>
-        </header>
-    )
-
-}
-
+  return (
+    <header className="bg-blue-700 h-20 text-white">
+      <div className="w-[80%] m-auto flex justify-between gap-10 items-center py-5">
+        <img className="h-10" src={logo} alt="Logo" />
+        <div className="w-1/3 inline-flex items-center">
+          {" "}
+          <input className="w-full rounded px-2 py-1 bg-gray-300" type="text" placeholder="search...."/>{" "}
+          <i class="fa-solid fa-magnifying-glass -ml-8 text-black cursor-pointer"></i>
+        </div>
+        <div className="flex gap-5 items-center md:w-1/5 ">
+          <button className="bg-gray-600 p-1 px-4 rounded text-xl font-bold">
+            Login
+          </button>
+          <div className="inline-flex gap-2 items-center hover:cursor-pointer">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <p>Cart</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
